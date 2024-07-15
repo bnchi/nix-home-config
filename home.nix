37a9f1718d72dd1f786de3e5e36e9cb7edab95b3
Bnchi <bnchifahmi@gmail.com>
2024-07-14 22:17:24 -0400

    updates

--- home.nix
+++ home.nix
@@ -13,14 +13,21 @@ in
 
 
 {
+  # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "noor";
-  home.homeDirectory = "/Users/noor";
+  home.username = "bnchi";
+  home.homeDirectory = "/home/bnchi";
 
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 
   home.packages = with pkgs; [
-    yarn
     nodejs
     nodePackages.typescript
     nodePackages.typescript-language-server
@@ -37,8 +44,6 @@ in
     htop
     nginx
     wrk
-    toybox
-    zld
   ];
 
   home.sessionVariables = {
@@ -49,7 +54,7 @@ in
   programs.zsh = { 
     enable = true;
     enableCompletion = true;
-    enableAutosuggestions = true;
+    autosuggestion.enable = true;
     shellAliases = {
       szsh = "source ~/.zshrc";
       nixswitch = "home-manager switch && source ~/.zshrc";
